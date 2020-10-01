af88bbf3b766f9236ea76ba7c69e034b317f5f6a
Đỗ Văn Huấn <59092467+huandv21-8@users.noreply.github.com>
2020-10-01 09:51:28 +0700

    update assignment 7

---
+++ lession1/Weather/Service/WeatherService.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Weather.Models;
+
+namespace Weather.Service
+{
+    class WeatherService
+    {
+        private readonly string stringUrl = String.Format
+            ("http://api.openweathermap.org/data/2.5/weather?q=hanoi,vn&appid=09a71427c59d38d6a34f89b47d75975c&units=metric"
+            );
+
+        public async Task<Root> GetData()
+        {
+            
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(stringUrl);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var stringContent = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<Root>(stringContent);
+                 
+            }
+            return null;
+        }
+    }
+}
